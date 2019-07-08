d0725d6df0d27d32ac509771c815b17f262d0754
iostiny <iostiny@yahoo.com>
2019-07-08 18:08:00 +0800

    Leap exercise

--- exercises/leap/lib/leap.dart
+++ exercises/leap/lib/leap.dart
@@ -1,3 +1,9 @@
 class Leap {
   // Put your code here
+  bool leapYear([int extraYear = 0]) {
+    if (extraYear % 4 != 0) return false;
+
+    return extraYear % 100 != 0 || extraYear % 400 == 0;
+  }
 }
+
